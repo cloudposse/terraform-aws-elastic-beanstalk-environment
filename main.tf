9de48a7aa5d0fad44911e9315f7c3148459c25ab
Andriy Knysh <aknysh@users.noreply.github.com>
2018-01-07 22:00:18 -0500

    Bump `terraform-null-label` version to `0.3.1` (#24)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Define composite variables for resources
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   name       = "${var.name}"
   stage      = "${var.stage}"
