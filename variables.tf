3d478e8d60a4c4709077f778509ed3f15bd615b7
Andriy Knysh <aknysh@users.noreply.github.com>
2018-01-07 19:18:19 -0500

    Add CI. Terraform fmt (#23)
    * Terraform fmt. Add CI
    
    * Update `README`

--- variables.tf
+++ variables.tf
@@ -1,92 +1,92 @@
 variable "namespace" {
-  default = "global"
+  default     = "global"
   description = "Namespace, which could be your organization name, e.g. 'cp' or 'cloudposse'"
 }
 
 variable "stage" {
-  default = "default"
+  default     = "default"
   description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
 }
 
 variable "delimiter" {
-  type    = "string"
-  default = "-"
+  type        = "string"
+  default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
 
 variable "attributes" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
   description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "name" {
-  default = "app"
+  default     = "app"
   description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "healthcheck_url" {
-  default = "/healthcheck"
+  default     = "/healthcheck"
   description = "Application Health Check URL. Elastic Beanstalk will call this URL to check the health of the application running on EC2 instances"
 }
 
 variable "notification_protocol" {
-  default = "email"
+  default     = "email"
   description = "Notification protocol"
 }
 
 variable "notification_endpoint" {
-  default = ""
+  default     = ""
   description = "Notification endpoint"
 }
 
 variable "notification_topic_arn" {
-  default = ""
+  default     = ""
   description = "Notification topic arn"
 }
 
 variable "notification_topic_name" {
-  default = ""
+  default     = ""
   description = "Notification topic name"
 }
 
 variable "loadbalancer_type" {
-  default = "classic"
+  default     = "classic"
   description = "Load Balancer type, e.g. 'application' or 'classic'"
 }
 
 variable "loadbalancer_certificate_arn" {
-  default = ""
+  default     = ""
   description = "Load Balancer SSL certificate ARN. The certificate must be present in AWS Certificate Manager"
 }
 
 variable "http_listener_enabled" {
-  default = "false"
+  default     = "false"
   description = "Enable port 80 (http)"
 }
 
 variable "ssh_listener_enabled" {
-  default = "false"
+  default     = "false"
   description = "Enable ssh port"
 }
 
 variable "ssh_listener_port" {
-  default = "22"
+  default     = "22"
   description = "SSH port"
 }
 
 variable "zone_id" {
-  default = ""
+  default     = ""
   description = "Route53 parent zone ID. The module will create sub-domain DNS records in the parent zone for the EB environment"
 }
 
 variable "config_source" {
-  default = ""
+  default     = ""
   description = "S3 source for config"
 }
 
 variable "security_groups" {
-  type = "list"
+  type        = "list"
   description = "List of security groups to be allowed to connect to the EC2 instances"
 }
 
@@ -99,12 +99,12 @@ variable "vpc_id" {
 }
 
 variable "public_subnets" {
-  type = "list"
+  type        = "list"
   description = "List of public subnets to place Elastic Load Balancer"
 }
 
 variable "private_subnets" {
-  type = "list"
+  type        = "list"
   description = "List of private subnets to place EC2 instances"
 }
 
@@ -113,42 +113,42 @@ variable "keypair" {
 }
 
 variable "updating_min_in_service" {
-  default = "1"
+  default     = "1"
   description = "Minimum count of instances up during update"
 }
 
 variable "updating_max_batch" {
-  default = "1"
+  default     = "1"
   description = "Maximum count of instances up during update"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default     = "t2.micro"
   description = "Instances type"
 }
 
 variable "autoscale_lower_bound" {
-  default = "20"
+  default     = "20"
   description = "Minimum level of autoscale metric to add instance"
 }
 
 variable "autoscale_upper_bound" {
-  default = "80"
+  default     = "80"
   description = "Maximum level of autoscale metric to remove instance"
 }
 
 variable "autoscale_min" {
-  default = "2"
+  default     = "2"
   description = "Minumum instances in charge"
 }
 
 variable "autoscale_max" {
-  default = "3"
+  default     = "3"
   description = "Maximum instances in charge"
 }
 
 variable "solution_stack_name" {
-  default = ""
+  default     = ""
   description = "Elastic Beanstalk stack, e.g. Docker, Go, Node, Java, IIS. [Read more](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html)"
 }
 
@@ -175,32 +175,33 @@ variable "alb_zone_id" {
     us-west-1      = "Z1LQECGX5PH1X"
     us-west-2      = "Z38NKT9BP95V3O"
   }
+
   description = "ALB zone id"
 }
 
 variable "tags" {
-  type    = "map"
-  default = {}
+  type        = "map"
+  default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
 }
 
 variable "env_default_key" {
-  default = "DEFAULT_ENV_%d"
+  default     = "DEFAULT_ENV_%d"
   description = "Default ENV variable key for Elastic Beanstalk `aws:elasticbeanstalk:application:environment` setting"
 }
 
 variable "env_default_value" {
-  default = "UNSET"
+  default     = "UNSET"
   description = "Default ENV variable value for Elastic Beanstalk `aws:elasticbeanstalk:application:environment` setting"
 }
 
 variable "env_vars" {
-  default = {}
-  type    = "map"
+  default     = {}
+  type        = "map"
   description = "Map of custom ENV variables to be provided to the Jenkins application running on Elastic Beanstalk, e.g. `env_vars = { JENKINS_USER = 'admin' JENKINS_PASS = 'xxxxxx' }`"
 }
 
 variable "tier" {
-  default = "WebServer"
+  default     = "WebServer"
   description = "Elastic Beanstalk Environment tier, e.g. ('WebServer', 'Worker')"
 }
