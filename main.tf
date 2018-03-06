e6acd0599ec9b73053d5ab41fe81c8ae2e2bb26b
Guillaume Delacour <gui@iroqwa.org>
2018-03-06 02:21:57 +0100

    Add custom metrics for CloudWatch (#32)

--- main.tf
+++ main.tf
@@ -571,6 +571,11 @@ resource "aws_elastic_beanstalk_environment" "default" {
     value     = "${var.loadbalancer_certificate_arn}"
   }
   setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "ConfigDocument"
+    value     = "${var.config_document}"
+  }
+  setting {
     namespace = "aws:elasticbeanstalk:application"
     name      = "Application Healthcheck URL"
     value     = "HTTP:80${var.healthcheck_url}"
