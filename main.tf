3a2d58b3064c03476992953ee7dcd7f42c6ce272
Maxime de Roucy <maxime.deroucy@oxalide.com>
2018-06-27 18:57:01 +0200

    fix: Environment tag update failed
    When we try to set the tags "Name" and "Namespace" the second
    deployement fail (the first one is ok).
    
    ```
    * module.front-elastic-beanstalk-environment.aws_elastic_beanstalk_environment.default: 1 error(s) occurred:
    
    * aws_elastic_beanstalk_environment.default: Error waiting for Elastic Beanstalk Environment (e-pkzfqk3igp) to become ready: 2 error(s) occurred:
    
    * 2018-06-27 16:54:27.56 +0000 UTC (e-pkzfqk3igp) : Service:AmazonCloudFormation, Message:No updates are to be performed.
    * 2018-06-27 16:54:28.178 +0000 UTC (e-pkzfqk3igp) : Environment tag update failed.
    ```
    
    With this patch we don't try to set those tags.

--- main.tf
+++ main.tf
@@ -341,7 +341,7 @@ resource "aws_elastic_beanstalk_environment" "default" {
 
   version_label = "${var.version_label}"
 
-  tags = "${module.label.tags}"
+  tags = "${merge(map("Stage", "${module.label.stage}"), var.tags)}"
 
   setting {
     namespace = "aws:ec2:vpc"
