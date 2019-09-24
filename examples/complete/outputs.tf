f0682ce2ea0f497e32c400f3707571116562c026
Igor Rodionov <goruha@gmail.com>
2019-09-24 18:57:39 +0600

    Updated Readme

---
+++ examples/complete/outputs.tf
@@ -0,0 +1,76 @@
+output "host" {
+  value = module.elastic_beanstalk_environment.host
+}
+
+output "id" {
+  value = module.elastic_beanstalk_environment.id
+}
+
+output "name" {
+  value = module.elastic_beanstalk_environment.name
+}
+
+output "security_group_id" {
+  value = module.elastic_beanstalk_environment.security_group_id
+}
+
+output "elb_dns_name" {
+  value = module.elastic_beanstalk_environment.elb_dns_name
+}
+
+output "elb_zone_id" {
+  value = module.elastic_beanstalk_environment.elb_zone_id
+}
+
+output "ec2_instance_profile_role_name" {
+  value = module.elastic_beanstalk_environment.ec2_instance_profile_role_name
+}
+
+output "tier" {
+  value = module.elastic_beanstalk_environment.tier
+}
+
+output "application" {
+  value = module.elastic_beanstalk_environment.application
+}
+
+output "setting" {
+  value = module.elastic_beanstalk_environment.setting
+}
+
+output "all_settings" {
+  value = module.elastic_beanstalk_environment.all_settings
+}
+
+output "cname" {
+  value = module.elastic_beanstalk_environment.cname
+}
+
+output "autoscaling_groups" {
+  value = module.elastic_beanstalk_environment.autoscaling_groups
+}
+
+output "instances" {
+  value = module.elastic_beanstalk_environment.instances
+}
+
+output "launch_configurations" {
+  value = module.elastic_beanstalk_environment.launch_configurations
+}
+
+output "load_balancers" {
+  value = module.elastic_beanstalk_environment.load_balancers
+}
+
+output "queues" {
+  value = module.elastic_beanstalk_environment.queues
+}
+
+output "triggers" {
+  value = module.elastic_beanstalk_environment.triggers
+}
+
+output "elb_load_balancers" {
+  value = module.elastic_beanstalk_environment.elb_load_balancers
+}
+
