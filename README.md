ad7892370d5c357ca879824e09d5753aa7c9a954
Andriy Knysh <aknysh@users.noreply.github.com>
2019-10-04 12:04:20 -0400

    Add `tags` to `aws_elastic_beanstalk_application`. Add `appversion_lifecycle` block (#16)
    * Add `tags` to `aws_elastic_beanstalk_application`
    
    * Add `appversion_lifecycle` block
    
    * Add `appversion_lifecycle` block

--- README.md
+++ README.md
@@ -62,6 +62,9 @@ Available targets:
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
+| appversion_lifecycle_delete_source_from_s3 | Whether to delete application versions from S3 source | bool | `false` | no |
+| appversion_lifecycle_max_count | The max number of application versions to keep | number | `1000` | no |
+| appversion_lifecycle_service_role_arn | The service role ARN to use for application version cleanup. If left empty, the `appversion_lifecycle` block will not be created | string | `` | no |
 | attributes | Additional attributes (e.g. `1`) | list(string) | `<list>` | no |
 | delimiter | Delimiter to be used between `name`, `namespace`, `stage`, etc. | string | `-` | no |
 | description | Elastic Beanstalk Application description | string | `` | no |
--- main.tf
+++ main.tf
@@ -8,7 +8,24 @@ module "label" {
   tags       = var.tags
 }
 
+locals {
+  // Remove `Name` tag from the map of tags because Elastic Beanstalk generates the `Name` tag automatically
+  // and if it is provided, terraform tries to recreate the application on each `plan/apply`
+  // https://github.com/terraform-providers/terraform-provider-aws/issues/3963
+  tags = { for t in keys(module.label.tags) : t => module.label.tags[t] if t != "Name" }
+}
+
 resource "aws_elastic_beanstalk_application" "default" {
   name        = module.label.id
   description = var.description
+  tags        = local.tags
+
+  dynamic "appversion_lifecycle" {
+    for_each = var.appversion_lifecycle_service_role_arn != "" ? ["true"] : []
+    content {
+      service_role          = var.appversion_lifecycle_service_role_arn
+      max_count             = var.appversion_lifecycle_max_count
+      delete_source_from_s3 = var.appversion_lifecycle_delete_source_from_s3
+    }
+  }
 }
