e46897c0d14d1a8cb038981da59de375ae8ef79f
PingXinh <pinhxinhxinh@PingXoai.local>
2020-07-25 21:42:33 +0700

    Edit additional_setting: resource

--- main.tf
+++ main.tf
@@ -900,7 +900,7 @@ resource "aws_elastic_beanstalk_environment" "default" {
       namespace = setting.value.namespace
       name      = setting.value.name
       value     = setting.value.value
-      resource  = ""
+      resource  = lookup(setting.value, "resource", "")
     }
   }
 
