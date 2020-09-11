f6316e2c2bbc534acc2235aa69938268bd5f277e
Anton <akachurin@kitewheel.com>
2020-09-11 17:17:53 -0400

    Add better sample

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -103,5 +103,10 @@ module "elastic_beanstalk_environment" {
 data "aws_iam_policy_document" "minimal_s3_permissions" {
   statement {
     sid = "AllowS3OperationsOnElasticBeanstalkBuckets"
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation"
+    ]
+    resources = ["*"]
   }
 }
