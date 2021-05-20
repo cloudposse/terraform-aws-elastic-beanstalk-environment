1fd94f02141790cd89acc7617f2789f6d780b2e7
SweetOps <vladimir.fess@gmail.com>
2021-05-20 23:07:39 +0300

    upd

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -79,8 +79,8 @@ module "elastic_beanstalk_environment" {
       from_port                = 0
       to_port                  = 65535
       protocol                 = "-1"
-      source_security_group_id = [module.vpc.vpc_default_security_group_id]
-      cidr_blocks              = null
+      cidr_blocks              = []
+      source_security_group_id = module.vpc.vpc_default_security_group_id
       description              = "Allow all inbound traffic from trusted Security Groups"
     },
   ]
