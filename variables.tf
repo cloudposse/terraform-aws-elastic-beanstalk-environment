5438e4c549eeae37534e5796532c4e7cca046e90
SweetOps <vladimir.fess@gmail.com>
2021-05-21 14:00:39 +0300

    upd security_groups var description

--- variables.tf
+++ variables.tf
@@ -83,7 +83,7 @@ variable "security_group_rules" {
 
 variable "security_groups" {
   type        = list(string)
-  description = "List of security groups to be allowed to connect to the EC2 instances"
+  description = "A list of Security Group IDs to associate with EC2 instances"
   default     = []
 }
 
