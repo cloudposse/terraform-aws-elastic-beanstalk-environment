90bd1fa86b100d9cafd2c91a896b2081da052ba1
florian0410 <florians36@gmail.com>
2021-06-15 18:01:26 +0200

    Add example for NLB

---
+++ examples/nlb/fixtures.us-east-2.tfvars
@@ -0,0 +1,98 @@
+region = "us-east-2"
+
+availability_zones = ["us-east-2a", "us-east-2b"]
+
+namespace = "eg"
+
+stage = "test"
+
+name = "elastic-beanstalk-env"
+
+description = "Test elastic-beanstalk-environment"
+
+tier = "WebServer"
+
+environment_type = "LoadBalanced"
+
+loadbalancer_type = "network"
+
+availability_zone_selector = "Any 2"
+
+instance_type = "t3.micro"
+
+autoscale_min = 1
+
+autoscale_max = 2
+
+wait_for_ready_timeout = "20m"
+
+
+force_destroy = true
+
+rolling_update_enabled = true
+
+rolling_update_type = "Health"
+
+updating_min_in_service = 0
+
+updating_max_batch = 1
+
+application_port = 80
+
+root_volume_size = 8
+
+root_volume_type = "gp2"
+
+autoscale_measure_name = "CPUUtilization"
+
+autoscale_statistic = "Average"
+
+autoscale_unit = "Percent"
+
+autoscale_lower_bound = 20
+
+autoscale_lower_increment = -1
+
+autoscale_upper_bound = 80
+
+autoscale_upper_increment = 1
+
+elb_scheme = "public"
+
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
+solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.17 running Docker 18.06.1-ce"
+
+version_label = ""
+
+dns_zone_id = "Z3SO0TKDDQ0RGG"
+
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+additional_settings = [
+  {
+    namespace = "aws:elasticbeanstalk:managedactions"
+    name      = "ManagedActionsEnabled"
+    value     = "false"
+  }
+]
+
+env_vars = {
+  "DB_HOST"         = "xxxxxxxxxxxxxx"
+  "DB_USERNAME"     = "yyyyyyyyyyyyy"
+  "DB_PASSWORD"     = "zzzzzzzzzzzzzzzzzzz"
+  "ANOTHER_ENV_VAR" = "123456789"
+}
+
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/environments-cfg-autoscaling-scheduledactions.html
+scheduled_actions = [
+  {
+    name            = "Refreshinstances"
+    minsize         = "1"
+    maxsize         = "2"
+    desiredcapacity = "2"
+    starttime       = "2015-05-14T07:00:00Z"
+    endtime         = "2016-01-12T07:00:00Z"
+    recurrence      = "*/20 * * * *"
+    suspend         = false
+  }
+]
---
+++ test/src/examples_nlb_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test the Terraform module in examples/nlb using Terratest.
+func TestExamplesNlb(t *testing.T) {
+	t.Parallel()
+
+	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
+	// in your AWS account
+	attributes := []string{strings.ToLower(random.UniqueId())}
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/nlb",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"fixtures.us-east-2.tfvars"},
+		Vars: map[string]interface{}{
+			"attributes": attributes,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the value of an output variable
+	vpcCidr := terraform.Output(t, terraformOptions, "vpc_cidr")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "172.16.0.0/16", vpcCidr)
+
+	// Run `terraform output` to get the value of an output variable
+	privateSubnetCidrs := terraform.OutputList(t, terraformOptions, "private_subnet_cidrs")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, []string{"172.16.0.0/19", "172.16.32.0/19"}, privateSubnetCidrs)
+
+	// Run `terraform output` to get the value of an output variable
+	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, []string{"172.16.96.0/19", "172.16.128.0/19"}, publicSubnetCidrs)
+
+	// Run `terraform output` to get the value of an output variable
+	expectedName := fmt.Sprintf("eg-test-elastic-beanstalk-env-%s", attributes[0])
+	elasticBeanstalkApplicationName := terraform.Output(t, terraformOptions, "elastic_beanstalk_application_name")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedName, elasticBeanstalkApplicationName)
+
+	// Run `terraform output` to get the value of an output variable
+	elasticBeanstalkEnvironmentName := terraform.Output(t, terraformOptions, "elastic_beanstalk_environment_name")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedName, elasticBeanstalkEnvironmentName)
+
+	// Run `terraform output` to get the value of an output variable
+	elasticBeanstalkEnvironmentHostname := terraform.Output(t, terraformOptions, "elastic_beanstalk_environment_hostname")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "elastic-beanstalk-env.testing.cloudposse.co", elasticBeanstalkEnvironmentHostname)
+}
