fdb2fa6f7419530893ebc15764a77e489c7bfe47
florian0410 <florian.silva@hotmail.fr>
2021-08-27 11:32:20 +0200

    Restore removed default iam policy Added count condition to it

--- main.tf
+++ main.tf
@@ -98,6 +98,13 @@ resource "aws_iam_role" "ec2" {
   }
 }
 
+resource "aws_iam_role_policy" "default" {
+  count  = module.this.enabled && var.instance_role_name == "" ? 1 : 0
+  name   = "${module.this.id}-eb-default"
+  role   = join("", aws_iam_role.ec2.*.id)
+  policy = data.aws_iam_policy_document.extended.json  
+}
+
 resource "aws_iam_role_policy_attachment" "web_tier" {
   count      = module.this.enabled && var.instance_role_name == "" ? 1 : 0
   role       = join("", aws_iam_role.ec2.*.name)
