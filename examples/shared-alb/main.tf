d24837f40befd72973347315bca374822370b4e7
florian0410 <florians36@gmail.com>
2021-12-29 17:05:07 +0100

    Disable access logs for alb to avoid conflicts

--- examples/shared-alb/main.tf
+++ examples/shared-alb/main.tf
@@ -28,6 +28,7 @@ module "alb" {
   version    = "0.36.0"
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.subnets.public_subnet_ids
+  access_logs_enabled = false
 }
 
 module "elastic_beanstalk_application" {
