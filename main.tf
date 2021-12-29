404dfd53cac75e1c47bd69cbecb9b351c972b306
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-12-29 14:56:23 +0000

    Auto Format

--- main.tf
+++ main.tf
@@ -488,14 +488,14 @@ locals {
       value     = var.loadbalancer_type
     },
   ]
-  
+
   # Settings for beanstalk managed elb only (so not for shared ALB)
   beanstalk_elb_settings = [
     {
       namespace = "aws:ec2:vpc"
       name      = "ELBSubnets"
       value     = join(",", sort(var.loadbalancer_subnets))
-    },    
+    },
     {
       namespace = "aws:elasticbeanstalk:environment:process:default"
       name      = "Port"
@@ -516,7 +516,7 @@ locals {
   # Select elb configuration depending on loadbalancer_type
   elb_settings_nlb        = var.loadbalancer_type == "network" ? concat(local.nlb_settings, local.generic_elb_settings, local.beanstalk_elb_settings) : []
   elb_settings_alb        = var.loadbalancer_type == "application" && ! var.loadbalancer_is_shared ? concat(local.alb_settings, local.generic_alb_settings, local.generic_elb_settings, local.beanstalk_elb_settings) : []
-  elb_settings_shared_alb = var.loadbalancer_type == "application" && var.loadbalancer_is_shared ? concat(local.shared_alb_settings, local.generic_alb_settings, local.generic_elb_settings) : []  
+  elb_settings_shared_alb = var.loadbalancer_type == "application" && var.loadbalancer_is_shared ? concat(local.shared_alb_settings, local.generic_alb_settings, local.generic_elb_settings) : []
   elb_setting_classic     = var.loadbalancer_type == "classic" ? concat(local.classic_elb_settings, local.generic_elb_settings, local.beanstalk_elb_settings) : []
 
   # If the tier is "WebServer" add the elb_settings, otherwise exclude them
