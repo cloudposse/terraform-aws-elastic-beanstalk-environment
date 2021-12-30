3a79abbf59bebd229b936f143b5b6e7efc7066a2
aknysh <andriy.knysh@gmail.com>
2021-12-30 10:03:07 -0500

    Updates

--- examples/shared-alb/versions.tf
+++ examples/shared-alb/versions.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
+      version = ">= 3.0"
     }
   }
 }
