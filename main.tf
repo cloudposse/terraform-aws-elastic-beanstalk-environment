5a46c6885178a6451089bc7897a4e3b8767c7f85
David <dgreenfield0@users.noreply.github.com>
2022-12-28 11:22:37 -0800

    SSM Fix from PR - Fix typo

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ resource "aws_ssm_activation" "ec2" {
   iam_role           = join("", aws_iam_role.ec2.*.id)
   registration_limit = var.autoscale_max
   tags               = module.this.tags
-  depends_on.        = [aws_elastic_beanstalk_environment.default]
+  depends_on         = [aws_elastic_beanstalk_environment.default]
 }
 
 data "aws_iam_policy_document" "default" {
