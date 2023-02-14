d4009cb34cc408eccb2a3d7f7d4ca63a1e187802
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2023-02-14 14:47:52 -0500

    [#98] Add a flag to redirect HTTP traffic to HTTPS (#218)
    * Add flag to redirect HTTP to HTTPS
    
    * Rename variables and add support for shared load balancers
    
    * Modify existing examples
    
    * Update variables description
    
    * Fix redirect host default value
    
    * Replace custom status code with a flag
    
    * Fix format
    
    * Fix bad reference to aws_elastic_beanstalk_environment
    
    * Refactor datasources
    
    * Change status_code variable type
    
    * Fix load_balancer_arn reference
    
    * Fix load_balancer_arn
    
    * Add loadbalancer_redirect_http_to_https to complete example
    
    * Add missing period at the end of the validation error message
    
    * Update README.md and docs/terraform.md
    
    * Bump required_version
    
    * Update docs/terraform.md
    
    * Update README file
    
    * Change AWS provider constraint
    
    * Change AWS version constraint inside the README.md file

--- main.tf
+++ main.tf
@@ -1170,3 +1170,31 @@ module "dns_hostname" {
 
   context = module.this.context
 }
+
+data "aws_lb_listener" "http" {
+  count             = local.enabled && var.loadbalancer_redirect_http_to_https ? 1 : 0
+  load_balancer_arn = var.loadbalancer_is_shared ? var.shared_loadbalancer_arn : one(aws_elastic_beanstalk_environment.default.0.load_balancers)
+  port              = var.application_port
+}
+
+resource "aws_lb_listener_rule" "redirect_http_to_https" {
+  count        = local.enabled && var.loadbalancer_redirect_http_to_https ? 1 : 0
+  listener_arn = one(data.aws_lb_listener.http.*.arn)
+  priority     = var.loadbalancer_redirect_http_to_https_priority
+
+  condition {
+    path_pattern {
+      values = var.loadbalancer_redirect_http_to_https_path_pattern
+    }
+  }
+
+  action {
+    type = "redirect"
+    redirect {
+      host        = var.loadbalancer_redirect_http_to_https_host
+      port        = var.loadbalancer_redirect_http_to_https_port
+      protocol    = "HTTPS"
+      status_code = var.loadbalancer_redirect_http_to_https_status_code
+    }
+  }
+}
