59f5a22efc4ed29c69e855e9e874cb2b7f8c92b9
Max Lobur <max_lobur@outlook.com>
2023-05-08 20:06:44 +0300

    fix tests

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   enabled          = module.this.enabled
   partition        = join("", data.aws_partition.current[*].partition)
-  elb_logs_enabled = local.enabled && var.tier == "WebServer" && var.environment_type == "LoadBalanced" && var.loadbalancer_type != "network" && !var.loadbalancer_is_shared ? 1 : 0
+  elb_logs_enabled = local.enabled && var.tier == "WebServer" && var.environment_type == "LoadBalanced" && var.loadbalancer_type != "network" && !var.loadbalancer_is_shared
 }
 
 data "aws_partition" "current" {
